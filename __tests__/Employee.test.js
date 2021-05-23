ca464cda36b3f6fdf9f229a9a4bb71b162962c4d
Amber Pratt <78624822+apratt7891@users.noreply.github.com>
2021-05-22 20:39:56 -0600

    employee test comments

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,8 +1,8 @@
-// using Employee constructor 
+// Employee constructor//
 
 const Employee = require('../lib/Employee');
 
-// creates employee object 
+// sets up the Employee object//
 test('creates an employee object', () => {
     const employee = new Employee();
 
@@ -11,24 +11,26 @@ test('creates an employee object', () => {
     expect(employee.email).toEqual(expect.any(String));
 });
 
-// gets id from getId() 
+// source of Employee name// 
 test('gets employee name', () => {
     const employee = new Employee();
 
     expect(employee.getName()).toEqual(expect.any(String));
 });
-
+// source of Employee ID// 
 test('gets employee ID', () => {
     const employee = new Employee();
 
     expect(employee.getId()).toEqual(expect.any(String));
 });
-
+// source of Employee email// 
 test('gets employee email', () => {
     const employee = new Employee();
 
     expect(employee.getEmail()).toEqual(expect.stringContaining(employee.email.toString()));
 });
+
+// sets Employee role as Employee// 
 test('sets role as Employee', () => {
     const employee = new Employee();
 
