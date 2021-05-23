9faf93980f62ce22ba6d95200f5a0555883b6533
Amber Pratt <78624822+apratt7891@users.noreply.github.com>
2021-05-22 20:13:23 -0600

    employee test

--- index.js
+++ index.js
@@ -1,14 +1,9 @@
 const fs = require('fs');
 
 
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-const gname = profileDataArgs[0];
-const github = profileDataArgs[1];
+const generatePage = require ('.src/generatePage');
 
-
-const generatePage = (userName, githubName) => `Name: ${userName}, Github: ${githubName}`;
-
-fs.writeFile('index.html', generatePage(gname, github), err => {
+fs.writeFile('index.html', generatePage(), err => {
     if (err) throw err;
   
     console.log('Portfolio complete! Check out index.html to see the output!');
