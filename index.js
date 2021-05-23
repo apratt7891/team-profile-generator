15bdd1959f2d7d370d57ac8fe45d94615b196ab4
Amber Pratt <78624822+apratt7891@users.noreply.github.com>
2021-05-23 09:17:40 -0600

    complete index

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 const inquirer = require('inquirer');
-const generatePage = require ('.src/generatePage');
+const generatePage = require ('./src/generatePage');
 
 const Manager = require('./lib/Manager');
 const Intern = require('./lib/Intern');
@@ -82,46 +82,134 @@ const teamEmployee = ()=> {
     return inquirer.prompt ([
         {
             type: 'input',
-            name: 
+            name: 'employee role',
+            message: "Choose the employee's role",
+            choices: ['Engineer', 'Intern']
+        },
+        {
+            type: 'input',
+            name: 'name',
+            message: "Enter Employee's name (Required)",
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log("Please enter the Employee's name");
+                    return false;
+                }
+            }
+            
+        },
+        {
+            type: 'input',
+            name: 'employee id',
+            message: "Enter the Employee's ID",
+            validate: idInput => {
+                if (idInput) {
+                    return true;
+                } else {
+                    console.log("Please enter the Employee's ID");
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: "Enter the Employee's email address.",
+            validate: emailInput => {
+                if (emailInput) {
+                    return true;
+                } else {
+                    console.log("Please enter the Employee's email address");
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'github',
+            message: "Enter the Employee's GitHub Username",
+            when: (input) => input.role === "Engineer",
+            validate: githubInput => {
+                if (githubInput) {
+                    return true;
+                } else {
+                     console.log("Please enter the Employee's GitHub Username!");
+                     return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'github',
+            message: "Enter the Intern's school name",
+            when: (input) => input.role === "Intern",
+            validate: schoolInput => {
+                if (schoolInput) {
+                    return true;
+                } else {
+                     console.log("Please enter the Intern's school name!");
+                     return false;
+                }
+            }
+
+        },
+        {
+            type: 'confirm',
+            name: 'confirmAdd',
+            message: "Are there more employee's to add to the team?",
+            default: false
         }
     ])
-}
+    .then(teamData => {
+        let {name, id, email, role, github, school, confirmAdd } = teamData;
+        let employee;
+
+        if (role === "Intern") {
+            employee = new Intern (name, id, email, school);
+            console.log(employee);
+        } else if (role === "Engineer") {
+            employee = new Engineer (name, id, email, github);
+            console.log(employee);
+        }
+
+        myTeamArray.push(employee);
+
+        if(confirmAdd) {
+            return addNewEmployee(myTeamArray);
+        } else {
+            return myTeamArray;
+        }
+    })
+};
 
 
 
 
 
 
+const writeFile = generatePage => {
+    fs.writeFile('./dist/index.html', generatePage, err => {
+    if (err) {
+        console.log(err);
+        return;
+    } else {
+        console.log('You have successfully created your Team Profile! Check out index.html to see the output!');
+    }    
+  })
+};  
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-fs.writeFile('index.html', generatePage(gname, github), err => {
-    if (err) throw err;
-  
-    console.log('Portfolio complete! Check out index.html to see the output!');
-  });
-
-
+teamManger()
+    .then(teamEmployee)
+    .then(myTeamArray => {
+        return generatePage(myTeamArray);
+    })
+    .then(pageHTML => {
+        return writeFile(pageHTML);
+      })
+    .catch(err => {
+        console.log(err);
+    });
 
   
